603a88b5710829473f9951178cb44ea3f4d59d4d
Mark Bates <mark@markbates.com>
2018-10-17 15:51:39 -0400

    allows for specify flags for a command

--- plugins/plugdeps/plugin.go
+++ plugins/plugdeps/plugin.go
@@ -6,9 +6,10 @@ import (
 
 // Plugin represents a Go plugin for Buffalo applications
 type Plugin struct {
-	Binary string `toml:"binary" json:"binary"`
-	GoGet  string `toml:"go_get,omitempty" json:"go_get,omitempty"`
-	Local  string `toml:"local,omitempty" json:"local,omitempty"`
+	Binary   string    `toml:"binary" json:"binary"`
+	GoGet    string    `toml:"go_get,omitempty" json:"go_get,omitempty"`
+	Local    string    `toml:"local,omitempty" json:"local,omitempty"`
+	Commands []Command `toml:"command,omitempty" json:"commands,omitempty"`
 }
 
 // String implementation of fmt.Stringer
